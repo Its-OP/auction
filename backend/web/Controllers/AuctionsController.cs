52fb54dacae3af15b7ebb63e837ef1f80c874af1
turoniol <turoniol29@gmail.com>
2024-02-10 16:11:49 +0200

    Added searching by name and sorting by min price

--- backend/web/Controllers/AuctionsController.cs
+++ backend/web/Controllers/AuctionsController.cs
@@ -16,7 +16,7 @@ public class AuctionsController : ControllerBase
     {
         _context = context;
     }
-    
+
     [HttpPost]
     [Route("{userId:int}")]
     [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(AuctionContract))]
@@ -24,10 +24,10 @@ public class AuctionsController : ControllerBase
     {
         if (!AuctionArgumentsAreValid(auctionArguments))
             return BadRequest("Arguments are invalid");
-        
+
         if (auctionArguments.Images.Count(x => x.Metadata.Type == ImageType.Thumbnail) != 1)
             return BadRequest("There must be exactly 1 thumbnail");
-        
+
         var images = auctionArguments.Images.Select(x => new Image(x.Metadata.Type, new ImageBody(x.Base64Body))).ToList();
 
         var host = await _context.Users.SingleAsync(x => x.Id == userId, token);
@@ -37,7 +37,7 @@ public class AuctionsController : ControllerBase
 
         return Ok(new AuctionContract(auction));
     }
-    
+
     [HttpPost]
     [Route("close/{auctionId:int}")]
     public async Task<IActionResult> CloseAuction([FromRoute] int auctionId, CancellationToken token)
@@ -45,39 +45,55 @@ public class AuctionsController : ControllerBase
         var auction = await _context.Auctions.SingleOrDefaultAsync(x => x.Id == auctionId, token);
         if (auction is null)
             return BadRequest("Auction not found");
-        
+
         auction.Close();
         _context.Auctions.Update(auction);
         await _context.SaveChangesAsync(token);
 
         return Ok();
     }
-    
+
     [HttpGet]
     [Route("")]
     [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(List<AuctionContract>))]
-    public async Task<IActionResult> GetAuctions(CancellationToken token, [FromQuery] int pageSize = 10, [FromQuery] int pageNumber = 1)
+    public async Task<IActionResult> GetAuctions(CancellationToken token, [FromQuery] int pageSize = 10, [FromQuery] int pageNumber = 1,
+        [FromQuery] string search = "", [FromQuery] string sort = "asc")
     {
         if (pageSize < 1)
             return BadRequest("Page size is invalid");
 
         pageSize = Math.Min(pageSize, 100);
-        
+
         if (pageNumber < 1)
             return BadRequest("Page number is invalid");
 
-        var auctions = await _context.Auctions.Select(x => new AuctionContract(x)).Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync(token);
+        var auctions = _context.Auctions.Where(x => x.Title.Contains(search));
 
-        return Ok(auctions);
+        if (sort == "asc")
+        {
+            auctions = auctions.OrderBy(x => x.MinPrice);
+        }
+        else if (sort == "desc")
+        {
+            auctions = auctions.OrderByDescending(x => x.MinPrice);
+        }
+        else
+        {
+            return BadRequest("Sorting order name is invalid. Allowed: 'asc', 'desc'.");
+        }
+
+        var res = await auctions.Select(x => new AuctionContract(x)).Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync(token);
+
+        return Ok(res);
     }
-    
+
     [HttpPut]
     [Route("update/{auctionId:int}")]
     public async Task<IActionResult> UpdateAuctionDetails([FromBody] AuctionArguments auctionArguments, int auctionId, CancellationToken token)
     {
         if (!AuctionArgumentsAreValid(auctionArguments))
             return BadRequest("Arguments are invalid");
-        
+
         var auction = await _context.Auctions.Include(x => x.Images).Include(x => x.Host).SingleOrDefaultAsync(x => x.Id == auctionId, token);
         if (auction is null)
             return BadRequest("Auction does not exist");
@@ -89,13 +105,13 @@ public class AuctionsController : ControllerBase
         auction.MinPrice = auctionArguments.MinPrice;
         auction.Title = auctionArguments.Title;
         auction.MinBidValue = auctionArguments.MinBidValue;
-        
+
         _context.Auctions.Update(auction);
         await _context.SaveChangesAsync(token);
 
         return Ok(new AuctionContract(auction));
     }
-    
+
     [HttpPut]
     [Route("gallery/{auctionId:int}/thumbnail")]
     public async Task<IActionResult> UpdateAuctionThumbnail([FromBody] ImageContract newThumbnail, int auctionId, CancellationToken token)
@@ -109,13 +125,13 @@ public class AuctionsController : ControllerBase
 
         var thumbnail = auction.Images.Single(x => x.Type == ImageType.Thumbnail);
         thumbnail.Body.Base64Body = newThumbnail.Base64Body;
-        
+
         _context.Images.Update(thumbnail);
         await _context.SaveChangesAsync(token);
 
         return Ok(new AuctionContract(auction));
     }
-    
+
     [HttpDelete]
     [Route("gallery/{auctionId:int}/image/{imageId:int}")]
     public async Task<IActionResult> DeleteGalleryImage(int auctionId, int imageId, CancellationToken token)
@@ -130,13 +146,13 @@ public class AuctionsController : ControllerBase
         var image = auction.Images.SingleOrDefault(x => x.Id == imageId);
         if (image is null)
             return BadRequest("Image does not exist");
-        
+
         _context.Images.Remove(image);
         await _context.SaveChangesAsync(token);
 
         return Ok(new AuctionContract(auction));
     }
-    
+
     [HttpPut]
     [Route("gallery/{auctionId:int}")]
     public async Task<IActionResult> AddGalleryImage([FromBody] ImageContract newThumbnail, int auctionId, CancellationToken token)
@@ -150,7 +166,7 @@ public class AuctionsController : ControllerBase
 
         var image = new Image(ImageType.Gallery, new ImageBody(newThumbnail.Base64Body));
         auction.Images.Add(image);
-        
+
         await _context.SaveChangesAsync(token);
 
         return Ok(new AuctionContract(auction));
